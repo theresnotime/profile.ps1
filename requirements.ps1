33464110bd68caf512c44075ef18c38d432150ce
TheresNoTime <sam@theresnotime.co.uk>
2022-06-22 10:07:30 +0100

    Add modules, there has to be a better way of doing this

---
+++ requirements.ps1
@@ -0,0 +1,5 @@
+Install-Module -Confirm -Name CaptureIT -RequiredVersion 1.0.1.47
+Install-Module -Confirm -Name oh-my-posh -RequiredVersion 7.85.2
+Install-Module -Confirm -Name posh-git -RequiredVersion 1.1.0
+Install-Module -Confirm -Name PackageManagement -RequiredVersion 1.4.7
+Install-Module -Confirm -Name PendingReboot -RequiredVersion 0.9.0.6
